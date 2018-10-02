6ba03b1249c41b0dde2787a4957560b6bdfa5bb6
yacklebeam <jacob@jmtroxel.io>
2018-10-02 16:29:32 -0700

    Updated RenderComponent to render the texture

--- Components/RenderComponent.cs
+++ Components/RenderComponent.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ProjectValkyrie.Managers;
 
 namespace ProjectValkyrie.Components
 {
@@ -6,14 +8,19 @@ namespace ProjectValkyrie.Components
     // Does not store the Texture itself, as this would be copied data
     class RenderComponent
     {
-        public RenderComponent()
-        {
+        private string textureName;
+        private long physicsId;
 
+        public RenderComponent(long id)
+        {
+            physicsId = id;
         }
 
-        public void Render(SpriteBatch s)
-        {
+        public string TextureName { get => textureName; set => textureName = value; }
 
+        public void Render(SpriteBatch sb)
+        {
+            if (textureName != "") sb.Draw(GameSession.Instance.AssetManager.getTexture(textureName), GameSession.Instance.PhysicsManager.ConvertToScreenCoordinates(GameSession.Instance.PhysicsManager.Get(physicsId).Position), Color.White);
         }
     }
 }
