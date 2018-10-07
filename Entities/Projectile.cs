74c8e682234d7ca8fb38c0534d13341d62e71c87
Navneeth <navnavsgotarocket@gmail.com>
2018-10-07 17:56:04 -0500

    Added Projectile Entity

---
+++ Entities/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using ValhallaEngine.Entities;
+using ValhallaEngine.Components;
+using ValhallaEngine.Math;
+
+namespace ProjectValkyrie.Entities
+{
+    class Projectile
+    {
+        private float damage;
+
+        public float Damage { get => damage; set => damage = value; }
+        public EntityType EntityFaction { get => entityFaction; set => entityFaction = value; }
+
+        private EntityType entityFaction;
+
+        public Projectile(long id) : base(id)
+        {
+            damage = 0.0f;
+
+
+
+        }
+
+        public override void OnEvent(long id)
+    }
+}
